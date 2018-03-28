918c3c7bd1b998aeb8e53ad4a6d2a43d43e32ade
kotpsot <zygmunt.justyna@gmail.com>
2018-03-28 11:49:51 +0100

    Items printer prints the header.

--- lib/items_printer.rb
+++ lib/items_printer.rb
@@ -9,33 +9,33 @@ class ItemsPrinter
     @days = days
   end
 
-end
-
-
-puts "OMGHAI!"
-items = [
-  Item.new(name="+5 Dexterity Vest", sell_in=10, quality=20),
-  Item.new(name="Aged Brie", sell_in=2, quality=0),
-  Item.new(name="Elixir of the Mongoose", sell_in=5, quality=7),
-  Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
-  Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
-  Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
-  Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
-  Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
-  # This Conjured item does not work properly yet
-  Item.new(name="Conjured Mana Cake", sell_in=3, quality=6), # <-- :O
-]
-
-days = 3
-
-gilded_rose = GildedRose.new items
-(0...days).each do |day|
-  puts "-------- day #{day} --------"
-  puts "name, sellIn, quality"
-  items.each do |item|
-    puts item
+  def print_items(items)
+    days = @days
+    (0...days).each do |day|
+      puts "-------- day #{day} --------"
+      puts "name, sellIn, quality"
+    #   items.each do |item|
+    #     puts item
+    #   end
+    #   puts ""
+    #   gilded_rose.update_quality
+    #   days += 1
+    end
   end
-  puts ""
-  gilded_rose.update_quality
-  days += 1
+
 end
+
+
+# puts "OMGHAI!"
+# items = [
+#   Item.new(name="+5 Dexterity Vest", sell_in=10, quality=20),
+#   Item.new(name="Aged Brie", sell_in=2, quality=0),
+#   Item.new(name="Elixir of the Mongoose", sell_in=5, quality=7),
+#   Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+#   Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+#   Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
+#   Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
+#   Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+#   # This Conjured item does not work properly yet
+#   Item.new(name="Conjured Mana Cake", sell_in=3, quality=6), # <-- :O
+# ]
--- spec/items_printer_spec.rb
+++ spec/items_printer_spec.rb
@@ -2,18 +2,44 @@ require 'items_printer'
 
 describe ItemsPrinter do
 
-  subject(:items_printer) { described_class.new(5)}
+  subject(:items_printer) { described_class.new(1)}
   subject(:items_printer_no_days) { described_class.new}
+  items = ["Justyna", 1, 2]
 
   context 'Initialization' do
     describe '#initialize' do
       it 'is initialized with a guven number of days' do
-        expect(items_printer.days).to eq 5
+        expect(items_printer.days).to eq 1
       end
       it 'has default number of days eq 3' do
         expect(items_printer_no_days.days).to eq 3
       end
     end
   end
- 
+
+  context 'Printing Items' do
+    describe '#print_items(items)' do
+      it 'prints information about the items' do
+        expect{items_printer.print_items(items)}.to output(
+          "-------- day 0 --------\n" +
+          "name, sellIn, quality\n"
+        ).to_stdout
+      end
+    end
+  end
+
+
+
 end
+
+
+
+# "+5 Dexterity Vest, 10, 20\n" +
+# 'Aged Brie, 2, 0' +
+# 'Elixir of the Mongoose, 5, 7' +
+# 'Sulfuras, Hand of Ragnaros, 0, 80' +
+# 'Sulfuras, Hand of Ragnaros, -1, 80' +
+# 'Backstage passes to a TAFKAL80ETC concert, 15, 20' +
+# 'Backstage passes to a TAFKAL80ETC concert, 10, 49' +
+# 'Backstage passes to a TAFKAL80ETC concert, 5, 49' +
+# 'Conjured Mana Cake, 3, 6'
